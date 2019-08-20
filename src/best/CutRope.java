519006ee7a94b1cc4b65f847428e7b89540ba814
HowieWang <howiewh@163.com>
2019-08-20 23:47:03 +0800

    优化剪绳子

--- src/best/CutRope.java
+++ src/best/CutRope.java
@@ -1,8 +1,10 @@
 package best;
 
+import utils.ArrayUtils;
+
 /**
  * 剑指 14
- * 一段长 n 米的绳子，剪成 m 段，m、n 都是整数，每段长度分别是 k1, k2 ... km，求 k1 * k2 * k3 ... km 的最大值；
+ * 一段长 n 米的绳子，剪成 m 段，m、n 都是整数，且大于 1，每段长度分别是 k1, k2 ... km，求 k1 * k2 * k3 ... km 的最大值；
  * <p>
  * 思考：
  * （1）贪心法
@@ -23,7 +25,6 @@ package best;
  * 注意：
  * 遇到 LeetCode 343
  * 给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
- * 这个题目稍微不同是至少剪两段，所以处理一下边界条件即可。
  */
 class CutRope {
     public static void main(String[] args) {
@@ -33,60 +34,32 @@ class CutRope {
     }
 
     public int soluteBest(int n) {
+        if (n <= 1) {
+            return 0;
+        }
         if (n == 2){
             return 1;
         }
         if (n == 3){
             return 2;
         }
+        // 加一是为了让 n = index
         int[] tmp = new int[n + 1];
-        return soluteBestInner(n, tmp);
+        for (int i = 1; i <= n; i++) {
+            tmp[i] = soluteBestInner(i, tmp);
+        }
+        return tmp[n];
     }
 
     private int soluteBestInner(int n, int[] tmp) {
-        int result = tmp[n];
-        // 已经计算过
-        if (result != 0) {
-            return result;
-        }
-        // 边界值
-        if (n <5){
-            tmp[n] = n;
-            return n;
-        }
-        // 正常值
-        int[] once = new int[n + 1];
-        for (int i = 2; i < n; i++) {
+        int[] once = new int[n];
+        once[0] = n;
+        // 求解 f(n) = i * f( n-i )
+        for (int i = 1; i < n; i++) {
             int t = n - i;
-            tmp[t] = soluteBestInner(t, tmp);
             once[i] = i * tmp[t];
         }
-        return max(once);
-    }
-
-    private int max(int[] data) {
-        if (data == null || data.length == 0) {
-            return 0;
-        }
-        int max = data[0];
-        for (int i = 1; i < data.length; i++) {
-            if (data[i] > max) {
-                max = data[i];
-            }
-        }
-        return max;
-    }
-
-    public int soluteGreedy(int n) {
-        if (n <= 4) {
-            return n;
-        }
-        int result = 1;
-        while (n >= 5) {
-            result *= 3;
-            n -= 3;
-        }
-        // 此时 n 只能是 4，3，2
-        return result * n;
+        tmp[n] = ArrayUtils.max(once);
+        return tmp[n];
     }
 }
--- src/utils/ArrayUtils.java
+++ src/utils/ArrayUtils.java
@@ -19,4 +19,17 @@ public class ArrayUtils {
         data[index1] = data[index2];
         data[index2] = i;
     }
+
+    public static int max(int[] data) {
+        if (data == null || data.length == 0) {
+            return 0;
+        }
+        int max = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] > max) {
+                max = data[i];
+            }
+        }
+        return max;
+    }
 }
